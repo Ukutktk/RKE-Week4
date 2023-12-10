c2e6add69b2f181b213f6000bbd36d720494c862
Uku <149318155+Ukutktk@users.noreply.github.com>
2023-12-10 21:40:32 +0200

    Add files via upload

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+Random rnd = new Random();
+
+//Console.WriteLine(rnd);
+int randomSum = 0;
+int myRandomNum = rnd.Next(0, 10); //0-10
+
+for(int i = 0; i < 12; i++)
+{
+    myRandomNum = rnd.Next(0, 10);
+    randomSum = myRandomNum + randomSum;
+    Console.WriteLine($"My random number is {myRandomNum}");
+}
+
+Console.WriteLine(randomSum);
